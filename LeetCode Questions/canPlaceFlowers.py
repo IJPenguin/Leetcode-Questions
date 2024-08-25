13925440f4005ea78a0d9b00a0fbb28c1f0b6373
Penguin <ishanjaiswal370@gmail.com>
2024-08-25 23:30:03 +0530

    25th aug

--- LeetCode Questions/canPlaceFlowers.py
+++ LeetCode Questions/canPlaceFlowers.py
@@ -4,26 +4,31 @@ from typing import List
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         
-        if n == 0:
+        for i in range(0, len(flowerbed)):
+            if flowerbed[i] == 0:
+                left = (i == 0) or (flowerbed[i - 1] == 0)
+                right = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+
+                if left and right:
+                    flowerbed[i] = 1
+                    n -= 1
+                
+        if n <= 0:
             return True
-        
-        if len(flowerbed) - 2 <= n:
-            return False
-        
-        if flowerbed[0] == 0 and flowerbed[1] == 0:
-            flowerbed[0] = 1
-            n -= 1
-        
-
-
+        return False
 
 
 # flowerbed = [0, 0, 1, 0, 1]
 # n = 1
-# flowerbed = [1, 0, 0, 0, 1, 0, 0]
-# n = 2
-flowerbed = [1, 0, 0, 0, 1]
+
+flowerbed = [1, 0, 0, 0, 1, 0, 0]
+n = 2
+
+flowerbed = [1]
 n = 1
-# flowerbed = [1]
+
+flowerbed = [1, 0, 0, 0, 1]
+n = 2
+
 s = Solution()
 print(s.canPlaceFlowers(flowerbed, n))
