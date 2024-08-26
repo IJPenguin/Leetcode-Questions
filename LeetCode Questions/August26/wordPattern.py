69dd266048850d0628efb05d960c40a8ff34621b
Penguin <ishanjaiswal370@gmail.com>
2024-08-27 01:30:25 +0530

    26th aug

--- LeetCode Questions/August26/wordPattern.py
+++ LeetCode Questions/August26/wordPattern.py
@@ -1,16 +1,35 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
+        d = {}
+        arr = s.split(" ")
+        pat = [*pattern]
+        if len(pat) != len(arr):
+            return False
 
-        pass
+        for string, p in zip(arr, pat):
+            print(d)
+            if string in d and d[string] == p:
+                continue
+            elif string not in d:
+                if p not in d.values():
+                    d[string] = p
+                    continue
+            return False
+
+        return True
 
 pattern = "abba"
-s = "dog cat cat dog"        
-
-pattern = "abba"
-s = "dog cat cat fish"
-
-pattern = "aaaa" 
 s = "dog cat cat dog"
 
-s = Solution()
-print(s.wordPattern(pattern, s))
+# pattern = "abba"
+# s = "dog cat cat fish"
+
+# pattern = "aaaa"
+# s = "dog cat cat dog"
+
+
+pattern = "aaa"
+s = "aa aa aa aa"
+
+sol = Solution()
+print(sol.wordPattern(pattern, s))
