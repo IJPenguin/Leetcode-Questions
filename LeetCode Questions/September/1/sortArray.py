b464942cdc29de64daef40d0d418b3d42d0d8efa
Penguin <ishanjaiswal370@gmail.com>
2024-09-01 23:50:43 +0530

    sep 1

--- LeetCode Questions/September/1/sortArray.py
+++ LeetCode Questions/September/1/sortArray.py
@@ -2,38 +2,38 @@ from typing import List
 
 # Quick Sort
 
-# class Solution:
-#     def sortArray(self, nums: List[int]) -> List[int]:
-#         if len(nums) <= 1:
-#             return nums
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
         
-#         pivot = nums[0]
-#         l = []
-#         r = []
+        pivot = nums[0]
+        l = []
+        r = []
 
-#         for i in range(1, len(nums)):
-#             if nums[i] > pivot:
-#                 r.append(nums[i])
-#             else:
-#                 l.append(nums[i])
+        for i in range(1, len(nums)):
+            if nums[i] > pivot:
+                r.append(nums[i])
+            else:
+                l.append(nums[i])
 
-#         res = self.sortArray(l) + [pivot] + self.sortArray(r)
-#         return res
+        res = self.sortArray(l) + [pivot] + self.sortArray(r)
+        return res
 
 # Insertion Sort
 
-class Solution:
-    def sortArray(self, nums:List[int]) -> List[int]:
-        for j in range(1, len(nums)):
-            key = nums[j]
+# class Solution:
+#     def sortArray(self, nums:List[int]) -> List[int]:
+#         for j in range(1, len(nums)):
+#             key = nums[j]
 
-            i = j - 1
+#             i = j - 1
 
-            while i >= 0 and nums[i] > key:
-                nums[i + 1] = nums[i]
-                i -= 1
-            nums[i + 1] = key
-        return nums
+#             while i >= 0 and nums[i] > key:
+#                 nums[i + 1] = nums[i]
+#                 i -= 1
+#             nums[i + 1] = key
+#         return nums
 
 # Merge Sort
 
