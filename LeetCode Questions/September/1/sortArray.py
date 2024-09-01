4f6c4f4300115e1efded37e305d9c599459e6c94
Penguin <ishanjaiswal370@gmail.com>
2024-09-01 23:50:29 +0530

    sep 1

--- LeetCode Questions/September/1/sortArray.py
+++ LeetCode Questions/September/1/sortArray.py
@@ -22,25 +22,25 @@ from typing import List
 
 # Insertion Sort
 
-# class Solution:
-#     def sortArray(self, nums:List[int]) -> List[int]:
-#         for j in range(1, len(nums)):
-#             key = nums[j]
+class Solution:
+    def sortArray(self, nums:List[int]) -> List[int]:
+        for j in range(1, len(nums)):
+            key = nums[j]
 
-#             i = j - 1
+            i = j - 1
 
-#             while i >= 0 and nums[i] > key:
-#                 nums[i + 1] = nums[i]
-#                 i -= 1
-#             nums[i + 1] = key
-#         return nums
+            while i >= 0 and nums[i] > key:
+                nums[i + 1] = nums[i]
+                i -= 1
+            nums[i + 1] = key
+        return nums
 
 # Merge Sort
 
-class Solution:
-    def sortArray(self, nums:List[int]) -> List[int]:
+# class Solution:
+#     def sortArray(self, nums:List[int]) -> List[int]:
         
-        return nums
+#         return nums
     
 nums = [5,2,3,1]
 print(Solution().sortArray(nums))
