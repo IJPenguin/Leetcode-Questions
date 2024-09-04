f03dc4726268851e97e5bf661d52a45ca204b643
Penguin <ishanjaiswal370@gmail.com>
2024-09-04 15:19:01 +0530

    sep 4

--- LeetCode Questions/September/3/productOfArrayExceptSelf.py
+++ LeetCode Questions/September/3/productOfArrayExceptSelf.py
@@ -1,18 +1,19 @@
 from typing import List
 
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         l = [1] * len(nums)
         r = [1] * len(nums)
         prev = nums[0]
 
-        for i in range(1 ,len(nums)):
-            l[i] = prev * l[i - 1] 
+        for i in range(1, len(nums)):
+            l[i] = prev * l[i - 1]
             prev = nums[i]
 
         prev = nums[-1]
-        for i in range(len(nums) - 2 , - 1, -1 ):
-            r[i] = prev * r[i +  1]
+        for i in range(len(nums) - 2, - 1, -1):
+            r[i] = prev * r[i + 1]
             prev = nums[i]
 
         res = [0] * len(nums)
@@ -22,9 +23,9 @@ class Solution:
         print(l)
         print(r)
         return res
-            
 
-nums = [-1,1,0,-3,3]
+
+nums = [-1, 1, 0, -3, 3]
 # nums = [1,2,3,4]
 s = Solution()
 print(s.productExceptSelf(nums))
