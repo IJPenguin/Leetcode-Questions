3d7d85df44790fc4ac7766fcfaae604d636156b4
Penguin <ishanjaiswal370@gmail.com>
2024-09-06 01:25:01 +0530

    sep 6

--- LeetCode Questions/September/6/secondLargest.py
+++ LeetCode Questions/September/6/secondLargest.py
@@ -1,3 +1,6 @@
+import random
+
+
 def secondLargest(arr):
     if len(arr) < 2:
         return -1
@@ -9,8 +12,13 @@ def secondLargest(arr):
             m2, m1 = m1, arr[i]
         elif arr[i] == m1:
             m2 = arr[i]
+        elif arr[i] > m2:
+            m2 = arr[i]
     return m2
 
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+arr = [random.randint(1, 100) for _ in range(10)]
+print(arr)
+print(sorted(arr))
 print(secondLargest(arr))
