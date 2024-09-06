24477057d2f867ea4f696aaa6297e71779e0b094
Penguin <ishanjaiswal370@gmail.com>
2024-09-06 23:39:12 +0530

    sep 6

--- LeetCode Questions/September/6/brickWall.py
+++ LeetCode Questions/September/6/brickWall.py
@@ -1,18 +1,29 @@
 from typing import List
+from collections import defaultdict
+
+
 class Solution:
     def leastBricks(self, wall: List[List[int]]) -> int:
-        if len(wall) == 1:
-            return 0
-        
-        val = sum(wall[0])
-        if val == 1:
-            return 0 
-        
-        
+        n = len(wall)
+        s = sum(wall[0])
+        hash = {}
+        for w in wall:
+            start = 0
+            for val in w:
+                start += val
+                if start != s:
+                    if start in hash:
+                        hash[start] += 1
+                    else:
+                        hash[start] = 1
+        if hash.values():
+            val = max(hash.values())
+        else:
+            return n
+        return n - val
 
-        
-        pass
 
-wall = [[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]]
+wall = [[1, 2, 2, 1], [3, 1, 2], [1, 3, 2], [2, 4], [3, 1, 2], [1, 3, 1, 1]]
+wall = [[2], [2], [2]]
 s = Solution()
 print(s.leastBricks(wall))
